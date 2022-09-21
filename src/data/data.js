3d7a4f4065d0c8fd28807edc137a3ed2af06ddb3
George Njogu <sbeveme@gmail.com>
2022-09-21 16:22:53 +0300

    assignment done

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,6 @@
 // use these variables in your JSX (update them with your own name and city!)
-const name = "Liza";
-const city = "New York";
+const name = "George Njogu";
+const city = "Kitui County";
 
 const image = "https://i.imgur.com/mV8PQxj.gif";
 
